ac15ae95fb5f891d631443d383178fa96b5763a5
Ilmar López <ilmarlopezr@hotmail.com>
2018-11-18 13:23:41 -0800

    server running work

--- server.go
+++ server.go
@@ -1,5 +1,95 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/Sirupsen/logrus"
+	dbx "github.com/go-ozzo/ozzo-dbx"
+	routing "github.com/go-ozzo/ozzo-routing"
+	"github.com/go-ozzo/ozzo-routing/auth"
+	"github.com/go-ozzo/ozzo-routing/content"
+	"github.com/go-ozzo/ozzo-routing/cors"
+	"github.com/go-ozzo/ozzo-routing/file"
+	_ "github.com/go-sql-driver/mysql"
+	"myhmp.org/activation-server/errors"
+	"myhmp.org/plimpton-restapi/app"
+	"myhmp.org/tpm-office-restapi/apis"
+	"myhmp.org/tpm-office-restapi/daos"
+	"myhmp.org/tpm-office-restapi/services"
+)
+
+func main() {
+	// load application configurations
+	if err := app.LoadConfig("./config"); err != nil {
+		panic(fmt.Errorf("Invalid application configuration: %s", err))
+	}
+
+	// load error messages
+	if err := errors.LoadMessages(app.Config.ErrorFile); err != nil {
+		panic(fmt.Errorf("Failed to read the error message file: %s", err))
+	}
+
+	// create the logger
+	logger := logrus.New()
+
+	// connect to the database
+	db, err := dbx.MustOpen("mysql", app.Config.DSN)
+	if err != nil {
+		panic(err)
+	}
+	db.LogFunc = logger.Infof
+
+	// wire up API routing
+	http.Handle("/", buildRouter(logger, db))
+
+	// start the server
+	address := fmt.Sprintf(":%v", app.Config.ServerPort)
+	logger.Infof("server %v is started at %v\n", app.Version, address)
+	panic(http.ListenAndServe(address, nil))
+}
+
+func buildRouter(logger *logrus.Logger, db *dbx.DB) *routing.Router {
+	router := routing.New()
+
+	router.To("GET, HEAD", "/ping", func(c *routing.Context) error {
+		c.Abort() // skip all other middlewares/handlers
+		return c.Write("OK " + app.Version)
+	})
+
+	router.Use(
+		app.Init(logger),
+		content.TypeNegotiator(content.JSON),
+		cors.Handler(cors.Options{
+			AllowOrigins: "*",
+			AllowHeaders: "*",
+			AllowMethods: "*",
+		}),
+		app.Transactional(db),
+	)
+
+	rg := router.Group("/v1")
+
+	// Auth
+	authDAO := daos.NewAuthDAO()
+	apis.ServeAuthResource(rg, services.NewAuthService(authDAO))
+
+	// rg.Post("/auth", apis.Auth(app.Config.JWTSigningKey))
+	rg.Use(auth.JWT(app.Config.JWTVerificationKey, auth.JWTOptions{
+		SigningMethod: app.Config.JWTSigningMethod,
+		TokenHandler:  apis.JWTHandler,
+	}))
+
+	// Serving Static Files
+	rg.Get("/files/*", file.Server(file.PathMap{
+		"/v1/files": "/tmp/",
+	}))
+
+	// login
+	// loginDAO := daos.NewLoginDAO()
+	// apis.ServeLoginResource(rg, services.NewLoginService(loginDAO))
+
+	// wire up more resource APIs here
+
+	return router
 }
